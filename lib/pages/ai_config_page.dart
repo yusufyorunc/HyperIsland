2ddc1633d16999d89053c894c24e15dabcf565e0
yusufyorunc <yusufyorunc65@gmail.com>
2026-04-02 21:25:47 +0300

    refactor(ui): remove unused imports and dead code

--- lib/pages/ai_config_page.dart
+++ lib/pages/ai_config_page.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../controllers/settings_controller.dart';
